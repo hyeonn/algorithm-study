4c2fa019f5d6c2048ae7f41841a4377364c8ee2c
2_hyeonney0_0 <34808817+hyeonn@users.noreply.github.com>
2021-08-22 23:59:04 +0900

    음양더하기
    음양더하기

---
+++ programmers/새 폴더/add_negative_positive_numbers.py
@@ -0,0 +1,11 @@
+# 음양더하기
+
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(signs)):
+        if signs[i] == False:
+            answer -= absolutes[i]
+        else:
+            answer += absolutes[i]
+
+    return answer
