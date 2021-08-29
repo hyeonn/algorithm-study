684c35133e365ca41835a1f8318189541f1b5d0a
2_hyeonney0_0 <34808817+hyeonn@users.noreply.github.com>
2021-08-29 23:57:58 +0900

    Binary Tree
    Binary Tree

---
+++ 자료구조/Binary_Tree.py
@@ -0,0 +1,65 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BinarySearchTree(Node):
+    def insert(self, data):
+        self.root = self._insert_value(self.root, data)
+        return self.root is not None
+
+    def _insert_value(self, node, data):
+        if node is None:
+            node = Node(data)
+        else:
+            if data <= node.data:
+                node.left = self._insert_value(node.left, data)
+            else:
+                node.right = self._insert_value(node.right, data)
+        return node
+    # 탐색
+    def find(self, key):
+        return self._find_value(self.root, key)
+
+    def _find_value(self, root, key):
+        if root is None or root.data == key:
+            return root is not None
+        elif key < root.data:
+            return self._find_value(root.left, key)
+        else:
+            return self._find_value(root.right, key)
+
+    def delete(self, key):
+        self.root, deleted = self._delete_value(self.root, key)
+        return deleted
+
+    def _delete_value(self, node, key):
+        if node is None:
+            return node, False
+
+        deleted = False
+        if key == node.data:
+            deleted = True
+            # 자식노드가 모두 존재할 경우
+            # !! 오른쪽 서브트리의 가장 왼쪽 아래 노드를 가져옴
+            if node.left and node.right:
+                parent, child = node, node.right
+                while child.left is not None:
+                    parent, child = child, child.left
+                child.left = node.left
+                if parent != node:
+                    parent.left = child.right
+                    child.right = node.right
+                node = child
+            # 자식 노드가 하나인 경우
+            elif node.left or node.right:
+                node = node.left or node.right
+            # 자식 노드가 없는 경우
+            else:
+                node = None
+        elif key < node.data:
+            node.left, deleted = self._delete_value(node.left, key)
+        else:
+            node.right, deleted = self._delete_value(node.right, key)
+        return node, deleted
