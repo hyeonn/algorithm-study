b6c50afe16d65e217a55c50982a8dfe29cccc16f
2_hyeonney0_0 <34808817+hyeonn@users.noreply.github.com>
2021-09-04 02:19:34 +0900

    10773 제로
    [스택] 10773 제로

---
+++ baekjoon/zero.py
@@ -0,0 +1,9 @@
+﻿n = int(input())
+stack = []
+for i in range(n):
+    stack.append(int(input()))
+    if stack[-1] == 0:
+        stack.pop(-1)
+        if len(stack)>=1:
+            stack.pop(-1)
+print(sum(stack))
