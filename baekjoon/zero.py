e487946705467723d4ff80e94e463cbbcec198ea
2_hyeonney0_0 <34808817+hyeonn@users.noreply.github.com>
2021-09-04 02:25:04 +0900

    Update zero.py

--- baekjoon/zero.py
+++ baekjoon/zero.py
@@ -1,9 +1,13 @@
 ﻿n = int(input())
 stack = []
+
 for i in range(n):
     stack.append(int(input()))
+    # 0일 때 스택에서 제거
     if stack[-1] == 0:
         stack.pop(-1)
+        # 0 이전 숫자 삭제
         if len(stack)>=1:
             stack.pop(-1)
+
 print(sum(stack))
