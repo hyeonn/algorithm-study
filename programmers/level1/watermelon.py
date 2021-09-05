800b3bbbf68a82536b6da1e96439c3dbc0fb5e36
2_hyeonney0_0 <34808817+hyeonn@users.noreply.github.com>
2021-09-05 23:46:42 +0900

    수박수박수박수박수박수
    수박수박수박수박수박수

---
+++ programmers/level1/watermelon.py
@@ -0,0 +1,7 @@
+def solution(n):
+    return '수박'*(n//2)+'수'*(n%2)
+    # watermelon = '수박'
+    # if n%2 == 0:
+    #     return watermelon*int(n/2)
+    # else:
+    #     return watermelon*int(n/2)+'수'
