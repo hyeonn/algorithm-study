48a3f7174997d73c9cf1b2dace5c567955589154
2_hyeonney0_0 <34808817+hyeonn@users.noreply.github.com>
2021-10-03 23:40:20 +0900

    Update Merge_sort.py

--- 자료구조/Merge_sort.py
+++ 자료구조/Merge_sort.py
@@ -1,17 +1,17 @@
 def merge_sort(arr):
     n = len(arr)
-    # 정렬할 리스트의 자료 개수가 한 개 이하이면 리턴
+    # 정렬할 리스트의 자료 개수가 1개 이하일 때 리턴
     if n <= 1:
         return arr
 
-    # 리스트를 반으로 나누어 각각 머지소트 호출
+    # 리스트를 반으로 나누어 각각 다시 머지소트
     mid = n // 2
     a = merge_sort(arr[:mid]) # 앞부분 정렬 (재귀)
     b = merge_sort(arr[mid:]) # 앞부분 정렬 (재귀)
 
     result = [] # merge된 결과 저장
 
-    # 두 리스트에 모두 자료가 남아있는 동안 반복
+    # 하나가 빌 때 까지 반복
     while a and b:
         # 작은 값을 리스트에 저장
         if a[0] < b[0]:
